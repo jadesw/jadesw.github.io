2578f15275643d2482730b2b981fb6477c7ea33d
serenity-wade <swade@g.hmc.edu>
2019-11-08 09:39:24 -0800

    color bashing

--- js/main.js
+++ js/main.js
@@ -12,5 +12,4 @@ function closeSidebar() {
     document.getElementById("mySidebar").style.width = "85px";
     document.getElementById("main").style.marginLeft = "85px";
     this.collapsed = true;
-    
  }
