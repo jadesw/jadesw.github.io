f34921e65ce9826a7e8468fdbde2e1f00a78d02e
serenity-wade <swade@g.hmc.edu>
2019-11-08 09:18:51 -0800

    troubleshoot sidebar

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var collapsed = true;
 
-function toggleSidebar() {
-    if (mini) {
-        console.log("Opening Sidebar")
-        document.getElementById("mySidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        this.collapsed = false;
-    } else {
-        console.log("Opening Sidebar")
-        document.getElementById("mySidebar").style.width = "85px";
-        document.getElementById("main").style.marginLeft = "85px";
-        this.collapsed = true;
-    }
+function openSidebar() {
+    console.log("Opening Sidebar")
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.collapsed = false;
+}
+function closeSidebar() {
+    console.log("Closing Sidebar")
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.collapsed = true;
+    
  }
